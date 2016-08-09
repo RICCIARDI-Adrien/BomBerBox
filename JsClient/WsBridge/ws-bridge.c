7d3bb8e857ec393c4b0a6cad94532c13ec5934a3
ybt <ybt@efixo.com>
2016-08-09 22:46:08 +0200

    clean ws-bridge: handle correctly clients deconnection

--- JsClient/WsBridge/ws-bridge.c
+++ JsClient/WsBridge/ws-bridge.c
@@ -19,13 +19,28 @@
 #define DBG_STR(buf)
 #endif
 
+// Max number of simultaneous client connections authorized
 #define MAX_BRIDGE_CONNECTIONS 50
 
-static int client_socket[MAX_BRIDGE_CONNECTIONS];
-static int client_nb = 0;
+/* Program structs */
+
+typedef struct {
+    int ws_fd;
+    int tcp_fd;
+} t_client;
+
+typedef struct {
+    unsigned int client_nb;
+    t_client client_socket[MAX_BRIDGE_CONNECTIONS];
+} t_ws_bridge;
+
+/* Global variables */
+
+static t_ws_bridge wsb;
+
+static bool dead = false;
 
 /* Signaling stuff to stop the server */
-static bool dead = false;
 
 void intHandler()
 {
@@ -68,11 +83,64 @@ int server_read(int fd, char* buffer,int bufferSize)
 }
 /* ----------------- */
 
+/* Add/Delete client functions */
+int add_client(int sock_fd)
+{
+    unsigned int i;
+
+    if(wsb.client_nb >= MAX_BRIDGE_CONNECTIONS)
+    {
+        printf("[%s:%d] Error : Maximum number of clients already connected.\n", __FUNCTION__, __LINE__);
+        return 1;
+    }
+
+    // Find the first free element into the list
+    for(i=0; i<MAX_BRIDGE_CONNECTIONS; i++)
+    {
+        if(wsb.client_socket[i].ws_fd == -1)
+        {
+            wsb.client_nb++;
+            wsb.client_socket[i].ws_fd = sock_fd;
+            printf("[%s:%d] Info : new client with id %d is connected.\n", __FUNCTION__, __LINE__, sock_fd);
+            printf("[%s:%d] Info : %d client(s) connected.\n", __FUNCTION__, __LINE__, wsb.client_nb);
+
+            return 0;
+        }
+    }
+
+    printf("[%s:%d] Error : Unknown error.\n", __FUNCTION__, __LINE__);
+
+    return 1;
+}
+
+int delete_client(int sock_fd)
+{
+    unsigned int i;
+
+    // Find fd to delete into wsb struct
+    for(i=0; i<MAX_BRIDGE_CONNECTIONS; i++)
+    {
+        if(wsb.client_socket[i].ws_fd == sock_fd)
+        {
+            wsb.client_nb--;
+            printf("[%s:%d] Info : client connection with id %d is terminated.\n", __FUNCTION__, __LINE__, sock_fd);
+            printf("[%s:%d] Info : %d client(s) connected.\n", __FUNCTION__, __LINE__, wsb.client_nb);
+            close(sock_fd);
+            wsb.client_socket[i].ws_fd = -1;
+            return 0;
+        }
+    }
+
+    printf("[%s:%d] Error : client to delete not found.\n", __FUNCTION__, __LINE__);
+    return 1;
+}
+
 /* Websocket thread handler */
 void *websocket_thread_handler(void *arg)
 {
-    int i, num_events;
-    int events_found = 0;
+    int num_events, ret;
+    unsigned int nb, i;
+    unsigned int events_found = 0;
     char client_ws_msg[1024], data[1024];
     struct pollfd pfds[MAX_BRIDGE_CONNECTIONS];
 
@@ -84,14 +152,20 @@ void *websocket_thread_handler(void *arg)
 
     while(!dead)
     {
-        for(i=0; i<client_nb; i++)
+        // Refresh list of fds to poll
+        nb = 0;
+        for(i=0; nb<wsb.client_nb; i++)
         {
-            pfds[i].fd = client_socket[i];
-            pfds[i].events = POLLIN | POLLPRI;
+            if(wsb.client_socket[i].ws_fd != -1)
+            {
+                pfds[nb].fd = wsb.client_socket[i].ws_fd;
+                pfds[nb].events = POLLIN | POLLPRI;
+                nb++;
+            }
         }
 
         // Poll the client sockets without blocking the thread
-        num_events = poll(pfds, client_nb, 0);
+        num_events = poll(pfds, wsb.client_nb, 0);
 
         if(num_events == -1)
         {
@@ -105,10 +179,27 @@ void *websocket_thread_handler(void *arg)
             {
                 if(pfds[i].revents & POLLIN)
                 {
+                    // Clear recv buffers
+                    client_ws_msg[0] = '\0';
+                    data[0] = '\0';
+
                     // Receive Ws message from client
-                    server_read(client_socket[i], client_ws_msg, sizeof(client_ws_msg));
-                    WEBSOCKET_get_content(client_ws_msg, sizeof(client_ws_msg), (unsigned char*) data, sizeof(data));
-                    printf("[%s:%d] Receive msg : %s\n", __FUNCTION__, __LINE__, data);
+                    server_read(pfds[i].fd, client_ws_msg, sizeof(client_ws_msg));
+                    ret = WEBSOCKET_get_content(client_ws_msg, sizeof(client_ws_msg), (unsigned char*) data, sizeof(data));
+                    if(ret == -2)
+                    {
+                        // Client disconnected
+                        delete_client(pfds[i].fd);
+                        pfds[i].fd = -1;
+                    }
+                    else if(ret == -1)
+                    {
+                        printf("[%s:%d] Unknown error get webSocket content\n", __FUNCTION__, __LINE__);
+                    }
+                    else
+                    {
+                        printf("[%s:%d] client id=%d Receive msg : %s\n", __FUNCTION__, __LINE__, pfds[i].fd, data);
+                    }
                     events_found++;
                 }
             }
@@ -122,19 +213,20 @@ void *websocket_thread_handler(void *arg)
 
 int server_wait_client(int sockfd)
 {
+    int cli_fd;
     struct sockaddr_in cli_addr;
     socklen_t clilen;
     char buffer[1024], response[1024];
 
-    client_socket[client_nb] = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if(client_socket[client_nb] == -1)
+    cli_fd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if(cli_fd == -1)
     {
         printf("[%s:%d] Error : connect() failed (%s).\n", __FUNCTION__, __LINE__, strerror(errno));
         return 1;
     }
 
     memset(buffer, 0, sizeof(buffer));
-    server_read(client_socket[client_nb], buffer, sizeof(buffer));
+    server_read(cli_fd, buffer, sizeof(buffer));
 
     if(WEBSOCKET_client_version(buffer) != 13)
     {
@@ -156,15 +248,18 @@ int server_wait_client(int sockfd)
     }
 
     // Send handshake response
-    server_write(client_socket[client_nb], response, strlen(response));
-    client_nb++;
-    printf("[%s:%d] Info : Ws client number %d connected.\n", __FUNCTION__, __LINE__, client_nb);
+    server_write(cli_fd, response, strlen(response));
+
+    // Add client to the list
+    if(add_client(cli_fd) != 0)
+        goto err;
 
     return 0;
 
 err:
-    server_write(client_socket[client_nb], WEBSOCKET_CONN_CLOSE, sizeof(WEBSOCKET_CONN_CLOSE));
-    close(client_socket[client_nb]);
+    // Close connection
+    server_write(cli_fd, WEBSOCKET_CONN_CLOSE, sizeof(WEBSOCKET_CONN_CLOSE));
+    close(cli_fd);
     return 1;
 }
 
@@ -191,6 +286,14 @@ int main(int argc, const char *argv[])
     // Remote TCP server to connect
     //remote_ip = argv[2];
     //remote_port = atoi(argv[3]);
+
+    // Init Server struct, clear all fds
+    wsb.client_nb = 0;
+    for(int i=0; i<MAX_BRIDGE_CONNECTIONS; i++)
+    {
+        wsb.client_socket[i].ws_fd = -1;
+        wsb.client_socket[i].tcp_fd = -1;
+    }
 
     // Create TCP socket dedicated to the bridge server
     bridge_sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -227,22 +330,19 @@ int main(int argc, const char *argv[])
     // register signal handler
     signal(SIGINT, intHandler);
 
-    // Wait for clients
-    while(!dead && client_nb < MAX_BRIDGE_CONNECTIONS)
+    // Start thread to handle client msg
+    if(pthread_create(&ws_thread , NULL, websocket_thread_handler, NULL) < 0)
     {
-        if(server_wait_client(bridge_sockfd) == 0)
-        {
-            /* 1st client connected => create Websocket thread to handle all clients messages */
-            if(client_nb == 1)
-            {
-                if(pthread_create(&ws_thread , NULL, websocket_thread_handler, NULL) < 0)
-                {
-                    printf("[%s:%d] Error : pthread_create() failed (%s).\n", __FUNCTION__, __LINE__, strerror(errno));
-                    return 1;
-                }
-                printf("[%s:%d] Info : WebSocket thread created.\n", __FUNCTION__, __LINE__);
-            }
-        }
+        printf("[%s:%d] Error : pthread_create() failed (%s).\n", __FUNCTION__, __LINE__, strerror(errno));
+        return 1;
+    }
+    printf("[%s:%d] Info : WebSocket thread created.\n", __FUNCTION__, __LINE__);
+
+
+    // Wait for clients
+    while(!dead)
+    {
+        server_wait_client(bridge_sockfd);
     }
 
     // Waiting thread terminating
@@ -258,7 +358,7 @@ int main(int argc, const char *argv[])
         return 1;
     }
 
-    printf("Succesfully exited program\n");
+    printf("[%s:%d] Info : Succesfully exited program\n", __FUNCTION__, __LINE__);
 
     return 0;
 }
