52e581fb09b5d4e6a4dd0323bc872d5894c0c05b
ybt <yannick.bilcot@sfr.com>
2016-08-22 18:04:13 +0200

    [JsClient] Use tile bitmap for all items displayed, add player shield item

--- JsClient/bomberbox.js
+++ JsClient/bomberbox.js
@@ -4,32 +4,34 @@ var ws;
 /* canvas context */
 var canvas;
 
+/* tile set png */
+var tile_set;
+
 /* client command definition */
 var action_type = {
-    NW_ACTION_DISPLAY_TILE : 0x0,
-    NW_ACTION_DISPLAY_STR  : 0x1
+    ACTION_DISPLAY_TILE : 0x0,
+    ACTION_DISPLAY_STR  : 0x1
 };
 
 /* server command definition */
 var command_type = {
-    NW_COMMAND_CONNECT : 0x2,
-    NW_COMMAND_INPUT   : 0x3
+    COMMAND_CONNECT : 0x2,
+    COMMAND_INPUT   : 0x3
 };
 
-/* img associated with tiles */
-var tile_img = {
-    GAME_TILE_ID_EMPTY : "sprites/ground.png",
-    GAME_TILE_ID_WALL : "sprites/block_solid.png",
-    GAME_TILE_ID_DESTRUCTIBLE_OBSTACLE : "sprites/block_explodable.png",
-    GAME_TILE_ID_CURRENT_PLAYER : "sprites/ninja_black.png",
-    GAME_TILE_ID_OTHER_PLAYER : "sprites/creep.png",
-    GAME_TILE_BOMB : "sprites/bomb.png",
-    GAME_TILE_EXPLOSION : "sprites/flame.png",
-    GAME_TILE_ITEM_GHOST : "",
-    GAME_TILE_ITEM_SHIELD : "sprites/item_shield.png",
-    GAME_TILE_ITEM_POWER_UP_BOMB_RANGE : "sprites/item_flame_powerup.png",
-    GAME_TILE_ITEM_POWER_UP_BOMBS_COUNT : "sprites/item_bomb_powerup.png"
-
+/* tile item coordonates */
+var tile_xy = {
+    TILE_ID_GROUND : {x: 192, y: 0},
+    TILE_ID_WALL : {x: 64, y: 0},
+    TILE_ID_DESTRUCTIBLE_WALL : {x: 32, y: 0},
+    TILE_ID_CURRENT_PLAYER : {x: 128, y:32},
+    TILE_ID_OTHER_PLAYER : {x: 128, y: 0},
+    TILE_BOMB : {x: 96, y: 0},
+    TILE_FLAME : {x: 160, y: 0},
+    TILE_PLAYER_SHIELD : {x: 224, y: 0},
+    TILE_ITEM_SHIELD : {x: 64, y: 32},
+    TILE_ITEM_POWER_UP_BOMB_RANGE : {x: 32, y: 32},
+    TILE_ITEM_POWER_UP_BOMBS_COUNT : {x: 0, y: 32}
 };
 
 /* Keyboard input value */
@@ -56,6 +58,9 @@ window.onload = function() {
         return;
     }
 
+    tile_set = new Image();
+    tile_set.src = "sprites/tile.png"
+
     document.addEventListener('keydown', keyboard_event);
 }
 
@@ -66,19 +71,16 @@ function bbb_console_log(str) {
 }
 
 function canvas_print_tile(tid, x, y) {
-    var img = new Image();
-    img.src = tile_img[Object.keys(tile_img)[tid]];
-    console.log(img.src + "x=" + x +" y=" + y);
-
-    img.onload = function() {
-        ctx.drawImage(img, x, y);
-    };
+    var tile_x = tile_xy[Object.keys(tile_xy)[tid]].x;
+    var tile_y = tile_xy[Object.keys(tile_xy)[tid]].y;
+    //console.log("tile x=" + tile_x + " y=" + tile_y + " x=" + x +" y=" + y);
+    ctx.drawImage(tile_set, tile_x, tile_y, 32, 32, x, y, 32, 32);
 }
 
 function send_kbd_msg(code) {
     if (typeof ws != 'undefined') {
         if(ws.readyState == ws.OPEN) {
-            var command = String.fromCharCode(command_type.NW_COMMAND_INPUT);
+            var command = String.fromCharCode(command_type.COMMAND_INPUT);
             ws.send(command + String.fromCharCode(code) + '\0');
         }
     }
@@ -119,7 +121,18 @@ function server_connect() {
     var name = form.player_name.value;
     var avatar = form.player_avatar.value;
 
-    tile_img.GAME_TILE_ID_CURRENT_PLAYER = "sprites/" + avatar + ".png";
+    if (avatar == "ninja_red") {
+        tile_xy.TILE_ID_CURRENT_PLAYER.x = 192;
+        tile_xy.TILE_ID_CURRENT_PLAYER.y = 32;
+    }
+    else if (avatar == "ninja_blue") {
+        tile_xy.TILE_ID_CURRENT_PLAYER.x = 160;
+        tile_xy.TILE_ID_CURRENT_PLAYER.y = 32;
+    }
+    else if (avatar == "alien") {
+        tile_xy.TILE_ID_CURRENT_PLAYER.x = 0;
+        tile_xy.TILE_ID_CURRENT_PLAYER.y = 0;
+    }
 
     bbb_console_log("Trying to connect to BomBerBox server.");
 
@@ -130,10 +143,10 @@ function server_connect() {
     ws.onmessage = function (evt) {
         var i = 0;
         do {
-            if (evt.data[i] == String.fromCharCode(action_type.NW_ACTION_DISPLAY_STR)) {
+            if (evt.data[i] == String.fromCharCode(action_type.ACTION_DISPLAY_STR)) {
                 bbb_console_log(evt.data.substring(i+2, i+2+evt.data.charCodeAt(i+1)));
                 i += evt.data.charCodeAt(i+1) + 2;
-            } else if(evt.data[i] == String.fromCharCode(action_type.NW_ACTION_DISPLAY_TILE)) {
+            } else if(evt.data[i] == String.fromCharCode(action_type.ACTION_DISPLAY_TILE)) {
                 var tid = evt.data.charCodeAt(i+1);
                 var x = evt.data.charCodeAt(i+3) * 32;
                 var y = evt.data.charCodeAt(i+2) * 32;
@@ -141,24 +154,24 @@ function server_connect() {
                 canvas_print_tile(tid, x, y);
             } else {
                 // Bad message: check next byte
-                console.log("bad msg!");
+                //console.log("bad msg!");
                 i++;
             }
         } while (i < evt.data.length);
     }
 
     ws.onerror = function () {
-        console.log("ws error!");
+        //console.log("ws error!");
     }
 
     ws.onopen = function () {
-        console.log("ws open!");
-        var command = String.fromCharCode(command_type.NW_COMMAND_CONNECT);
+        //console.log("ws open!");
+        var command = String.fromCharCode(command_type.COMMAND_CONNECT);
         ws.send(command + name + '\0');
     };
 
     ws.onclose = function (evt) {
-        console.log("ws closed!");
+        //console.log("ws closed!");
         // abnormal socket closure, maybe handcheck failed ...
         if (evt.code == 1006) {
             // try to reconnect
